7f9471b750cd4b3e9aecc8713e7be3b145168913
Rajat Sinha <rajat.sinha1000@gmail.com>
2022-04-17 23:59:06 +0530

    initial commit

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,60 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setloading] = useState(true)
+  const [tours, setTours] = useState([])
+  
+  const removeTour = (id) => {
+    const newTour = tours.filter((tour) => tour.id !== id)
+    setTours(newTour)
+  }
+
+  const fetchTours = async () => {
+    setloading(true)
+
+    try {
+      const response = await fetch(url)
+    const tours = await response.json()
+    setloading(false)
+    setTours(tours)
+    } catch (error) {
+      setloading(false)
+      console.log(error);
+    }
+
+    
+    
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+
+  
+  if(loading){
+    return (
+    <main>
+      <Loading/>
+    </main>
+    )
+  }
+  if(tours.length === 0){
+    return (
+      <main>
+        <div className='title'>
+          <h2>No tours left</h2>
+          <div className='underline' ></div>
+          <button className='btn' onClick={fetchTours}>refresh</button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+
+    </main>
+  )
 }
 
 export default App
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit doloribus cumque obcaecati magni doloremque harum vero quas placeat unde, eaque veniam voluptatibus, incidunt itaque. Harum nam sapiente, eos voluptatum molestiae dicta recusandae, corporis temporibus enim amet iure natus? Repudiandae, officia.
+      </p>
+    </section>
   )
 }
 
